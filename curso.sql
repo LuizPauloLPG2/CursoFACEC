6e90402b07530b3e33d9df0caa3b0dc1a089ae78
Luiz Paulo <37484860+LuizPauloLPG2@users.noreply.github.com>
2019-07-02 18:54:11 -0300

    Add files via upload

---
+++ curso.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 02-Jul-2019 às 23:53
+-- Versão do servidor: 10.3.16-MariaDB
+-- versão do PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `curso`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `table_cliente`
+--
+
+CREATE TABLE `table_cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email_cliente` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `biografia` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` char(1) COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` varchar(14) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `table_cliente`
+--
+
+INSERT INTO `table_cliente` (`id_cliente`, `nome_cliente`, `email_cliente`, `biografia`, `status`, `cpf`) VALUES
+(1, 'Luiz', 'luiz@gmail.com', NULL, 'A', '182.576.750-59'),
+(2, 'Paulo', 'paulo@gmail.com', NULL, 'I', '182.576.750-59');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `table_cliente`
+--
+ALTER TABLE `table_cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `table_cliente`
+--
+ALTER TABLE `table_cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
